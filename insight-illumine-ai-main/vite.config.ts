2a2065efc177bc96a13011f4597e3aaa8851ac09
jain christo <jainchristo01@gmail.com>
2026-03-30 06:08:43 +0530

    Corrected base path to match repo name

--- insight-illumine-ai-main/vite.config.ts
+++ insight-illumine-ai-main/vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/insight-illumine-ai-main/" : "/",
+  base: mode === "production" ? "/lumina-ai/" : "/",
   server: {
     host: "::",
     port: 8080,
